7531c932f7a378e7310605a7e8ec753c4684fee6
haowang <yousangdandan@yeah.net>
2020-11-02 12:10:30 +0800

    add timer demo code

--- cmd/taskTimerDemo.go
+++ cmd/taskTimerDemo.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-code-package/packages/task/timer"
+	"time"
 )
 
 func main() {
 	go timer.TaskHour()
+	fmt.Println("1111")
 	go timer.TaskMinute()
+	fmt.Println("2222")
 	go timer.TaskSecond()
+	fmt.Println("3333")
+	time.Sleep(70 * time.Minute)
 }
